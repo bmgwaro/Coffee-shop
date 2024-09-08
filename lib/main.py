0857a5972099468d57178b70872b3c88d55fce14
bmgwaro <brian.gwaro@student.moringaschool.com>
2024-09-08 22:36:56 +0300

    final commit

--- lib/main.py
+++ lib/main.py
@@ -15,13 +15,13 @@ order2 = Order(customer2, 4.0, coffee1)
 order3 = Order(customer3, 5.0, coffee2)  
 order4 = Order(customer1, 2.5, coffee1)  
 
-# Test Coffee class methods
-print("Orders for Espresso:", coffee1.orders())  # Should show orders associated with Espresso
-print("Customers who ordered Espresso:", [customer.name for customer in coffee1.customers()])  # Should list Alice, Bob
-print("Number of orders for Espresso:", coffee1.num_orders())  # Should return 3
-print("Average price for Espresso:", coffee1.average_price())  # Should return the average price of 3.5, 4.0, and 2.5
 
-print("Orders for Latte:", coffee2.orders())  # Should show orders associated with Latte
-print("Customers who ordered Latte:", [customer.name for customer in coffee2.customers()])  # Should list Charlie
-print("Number of orders for Latte:", coffee2.num_orders())  # Should return 1
-print("Average price for Latte:", coffee2.average_price())  # Should return 5.0
+print("Orders for Espresso:", coffee1.orders())  
+print("Customers who ordered Espresso:", [customer.name for customer in coffee1.customers()])  
+print("Number of orders for Espresso:", coffee1.num_orders())  
+print("Average price for Espresso:", coffee1.average_price())  
+
+print("Orders for Latte:", coffee2.orders())  
+print("Customers who ordered Latte:", [customer.name for customer in coffee2.customers()])  
+print("Number of orders for Latte:", coffee2.num_orders())  
+print("Average price for Latte:", coffee2.average_price())  
